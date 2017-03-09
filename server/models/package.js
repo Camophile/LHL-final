e304b3db42649f8e55962ef96392864027a76454
Arman Kazemi <kas1er@hotmail.com>
2017-03-09 01:47:47 +0000

    ironing out redundenciesin shelter-confirm page

--- server/models/package.js
+++ server/models/package.js
@@ -40,65 +40,10 @@ export function deliveryValidate(data) {
 
 export function getScheduledPackages() {
 
-  // return qb.where(knex.raw(query));
-
   return Package.forge({id: 1})
-  // .fetchAll().then(function(x) {
-  //   console.log("Package fetchAll", x.toJSON())
-  // })
+
   .fetch({ withRelated: ['shelter', 'box', 'user', 'grocery'] })
   .then(function(packages) {
-    console.log(packages.toJSON());
-    return packages.toJSON();
-  }).catch(function(err) {
-    console.error(err);
-  });
-}import bookshelf from '../bookshelf';
-import Shelter from './Shelter';
-import User from './User';
-import Box from './Box';
-import Grocery from './grocery';
-
-const Package = bookshelf.Model.extend({
-  tableName: 'package',
-  shelter: function() {
-    return this.belongsTo(Shelter);
-  },
-  user() {
-    return this.belongsTo(User, 'users_id');
-  },
-  box: function() {
-    return this.hasMany(Box)
-  },
-  grocery: function(){
-    return this.hasOne(Grocery).through(Box, 'id');
-  }
-});
-
-export default Package;
-
-export function insertPackages(data) {
-  return Package.forge({
-    shelter_id: data.shelter,
-    users_id: data.user,
-  }, { hasTimestamps: true }).save()
-}
-
-export function deliveryValidate(data) {
-  return Package.query({
-    where: { id: 2 }
-    }).save(
-    { delivered_at: new Date().toISOString() },
-    { method: 'update' }
-  );
-}
-
-export function getScheduledPackages() {
-
-  return Package.forge({id: 1})
-  .fetch({ withRelated: ['shelter', 'box', 'user', 'grocery'] })
-  .then(function(packages) {
-    console.log(packages.toJSON());
     return packages.toJSON();
   }).catch(function(err) {
     console.error(err);
